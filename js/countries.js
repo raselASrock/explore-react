2de1e657d513935e6a696aac0056c64fa7821c28
Rasel <mdraselm413@gmail.com>
2022-09-19 01:07:59 +0600

    country api

--- js/countries.js
+++ js/countries.js
@@ -5,6 +5,18 @@ const loadCountries = () =>{
     .then(data => displayCountries(data))
 }
 const displayCountries = countries => {
-    console.log(countries);
+    console.log(countries[0]);
+    const countriesHTML = countries.map(country => getCountryHTML(country));
+    const container = document.getElementById('countries');
+    container.innerHTML = countriesHTML.join(' ');
+    // console.log(countriesHTML);
+}
+const getCountryHTML = country =>{
+    return `
+    <div>
+        <img src="${country.flags.png}">
+        <h2> ${country.name} </h2>
+    </div>
+    `
 }
 loadCountries()
